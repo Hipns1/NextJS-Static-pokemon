7c690b7c473742111b771ddae9a5a438ac07408c
Hipns1 <jesudpf21@hotmail.com>
2022-06-24 14:16:38 -0500

    SSRG

--- utils/getPokeInfo.ts
+++ utils/getPokeInfo.ts
@@ -2,10 +2,20 @@ import { pokeAPI } from "../api"
 import { Pokemon } from "../interfaces"
 
 export const getPokemonInfo = async (query: string) => {
-    const { data } = await pokeAPI.get<Pokemon>(`/pokemon/${query}`)
-    return {
-        id: data.id,
-        name: data.name,
-        sprites: data.sprites
+
+    try {
+        const { data } = await pokeAPI.get<Pokemon>(`/pokemon/${query}`)
+
+        return {
+            id: data.id,
+            name: data.name,
+            sprites: data.sprites
+        }
+
+    } catch (error) {
+        return null;
     }
+
+
+
 }
